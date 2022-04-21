b08a9b7b8393ec9f8ff4ddd2b2a3455c0bcbc59d
dunja <dunjabogdanovic00@gmail.com>
2022-04-21 14:36:12 +0200

    patient model modified

--- projekat/projekat/Model/Patient.cs
+++ projekat/projekat/Model/Patient.cs
@@ -62,65 +62,9 @@ namespace Model
 
 
 
-        /// <summary>
-        /// Property for collection of Allergen
-        /// </summary>
-        /// <pdGenerated>Default opposite class collection property</pdGenerated>
-        public System.Collections.Generic.List<Allergen> Allergen
-      {
-         get
-         {
-            if (allergen == null)
-               allergen = new System.Collections.Generic.List<Allergen>();
-            return allergen;
-         }
-         set
-         {
-            RemoveAllAllergen();
-            if (value != null)
-            {
-               foreach (Allergen oAllergen in value)
-                  AddAllergen(oAllergen);
-            }
-         }
-      }
-      
-      /// <summary>
-      /// Add a new Allergen in the collection
-      /// </summary>
-      /// <pdGenerated>Default Add</pdGenerated>
-      public void AddAllergen(Allergen newAllergen)
-      {
-         if (newAllergen == null)
-            return;
-         if (this.allergen == null)
-            this.allergen = new System.Collections.Generic.List<Allergen>();
-         if (!this.allergen.Contains(newAllergen))
-            this.allergen.Add(newAllergen);
-      }
-      
-      /// <summary>
-      /// Remove an existing Allergen from the collection
-      /// </summary>
-      /// <pdGenerated>Default Remove</pdGenerated>
-      public void RemoveAllergen(Allergen oldAllergen)
-      {
-         if (oldAllergen == null)
-            return;
-         if (this.allergen != null)
-            if (this.allergen.Contains(oldAllergen))
-               this.allergen.Remove(oldAllergen);
-      }
-      
-      /// <summary>
-      /// Remove all instances of Allergen from the collection
-      /// </summary>
-      /// <pdGenerated>Default removeAll</pdGenerated>
-      public void RemoveAllAllergen()
-      {
-         if (allergen != null)
-            allergen.Clear();
-      }
+
+
+   
    
    }
 }
