83295f3f23322623507574d2d58ae5db9fb25f8b
Nenad <wotnenad@gmail.com>
2022-04-23 20:11:59 +0200

    zaboravio sam da se prebacim na granu upravnik-rasporedjivanje

---
+++ projekat/projekat/Model/RoomEquipmentDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace projekat.Model
+{
+    public class RoomEquipmentDTO
+    {
+        public uint Id { get; set; }
+
+        public uint RoomId { get; set; }
+
+        public string RoomName { get; set; }
+
+        public RoomType Type { get; set; }
+
+        public uint EquipmentId { get; set; }
+
+        public string EquipmentName { get; set; }
+
+        public uint Quantity { get; set; }
+
+        public RoomEquipmentDTO()
+        {
+
+        }
+
+        public RoomEquipmentDTO(uint id)
+        {
+            Id = id;
+        }
+
+        public RoomEquipmentDTO(uint id, uint roomId, string roomName, RoomType type, uint equipmentId, string equipmentName, uint quantity) : this(id)
+        {
+            RoomId = roomId;
+            RoomName = roomName;
+            Type = type;
+            EquipmentId = equipmentId;
+            EquipmentName = equipmentName;
+            Quantity = quantity;
+        }
+
+        public RoomEquipmentDTO(uint roomId, string roomName, RoomType type, uint equipmentId, string equipmentName, uint quantity) : this(roomId)
+        {
+            RoomName = roomName;
+            Type = type;
+            EquipmentId = equipmentId;
+            EquipmentName = equipmentName;
+            Quantity = quantity;
+        }
+    }
+}
