60f557cd24a12cfe425cee48ce3de4b1147e9c89
Nenad <wotnenad@gmail.com>
2022-04-23 14:03:00 +0200

    last control point added

--- projekat/projekat/View/ModelView/RoomView.xaml.cs
+++ projekat/projekat/View/ModelView/RoomView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,37 @@ namespace projekat.View.ModelView
     /// </summary>
     public partial class RoomView : Window
     {
+        private RoomControler _roomController;
+        private uint _id;
+        private uint _squareFootage;
+        private string _name;
+        private RoomType _roomType;
+
+        public ObservableCollection<Room> Data { get; set; }
+
+
         public RoomView()
         {
             InitializeComponent();
+            var app = Application.Current as App;
+            _roomController = app.RoomControler;
+   
+
+            Data = new ObservableCollection<Room>(_roomController.GetAll().ToList());
+
+
+
+            for (int i = 0; i < Data.Count(); i++)
+            {
+                DataGridXAML.Items.Add(Data[i]);
+            }
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+        
+
         }
     }
 }
